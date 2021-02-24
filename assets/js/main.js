8b0d4cc054bf5ac9d714e84c61c163e2e6d7b143
Andrea Silvano <andrea.silvano97@gmail.com>
2021-02-24 10:43:50 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,71 @@
+
+  var vid = document.getElementById('vid');
+
+function unmute(){
+  if(vid.muted){
+  vid.muted = false;
+}};
+
+  function play1(){
+    vid.removeAttribute("src");
+    vid.setAttribute("src",'assets/video/01.mp4');
+    goback1();
+
+  };
+  function play2(){
+    vid.removeAttribute("src");
+    vid.setAttribute("src",'assets/video/02.mp4');
+    goback2();
+
+  };
+  function play3(){
+    vid.removeAttribute("src");
+    vid.setAttribute("src",'assets/video/03.mp4');
+    goback3();
+
+  };
+  function play4(){
+    vid.removeAttribute("src");
+    vid.setAttribute("src",'assets/video/04.mp4');
+    goback3();
+
+  };
+
+  function goback1(){
+  vid.removeAttribute("loop");
+  vid.removeAttribute("muted");
+  unmute();
+  vid.onended = function() {
+  vid.removeAttribute("src");
+  vid.setAttribute("src",'assets/video/00.mp4',);
+
+}};
+function goback2(){
+vid.removeAttribute("loop");
+vid.removeAttribute("muted");
+unmute();
+vid.onended = function() {
+vid.removeAttribute("src");
+vid.setAttribute("src",'assets/video/00.mp4',);
+
+}};
+
+function goback3(){
+vid.removeAttribute("loop");
+vid.removeAttribute("muted");
+unmute();
+vid.onended = function() {
+vid.removeAttribute("src");
+vid.setAttribute("src",'assets/video/00.mp4',);
+
+}};
+
+function goback4(){
+vid.removeAttribute("loop");
+vid.removeAttribute("muted");
+unmute();
+vid.onended = function() {
+vid.removeAttribute("src");
+vid.setAttribute("src",'assets/video/00.mp4',);
+
+}};
