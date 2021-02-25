f9e50a43ddc814a95ea0251d2fbbb60b89ca2838
ndrcldrll <andre.barbosa@mail.polimi.it>
2021-02-25 17:31:15 +0100

    sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,71 +1,19 @@
-
-  var vid = document.getElementById('vid');
-
-function unmute(){
-  if(vid.muted){
-  vid.muted = false;
-}};
-
-  function play1(){
-    vid.removeAttribute("src");
-    vid.setAttribute("src",'assets/video/01.mp4');
-    goback1();
-
-  };
-  function play2(){
-    vid.removeAttribute("src");
-    vid.setAttribute("src",'assets/video/02.mp4');
-    goback2();
-
-  };
-  function play3(){
-    vid.removeAttribute("src");
-    vid.setAttribute("src",'assets/video/03.mp4');
-    goback3();
-
-  };
-  function play4(){
-    vid.removeAttribute("src");
-    vid.setAttribute("src",'assets/video/04.mp4');
-    goback3();
-
-  };
-
-  function goback1(){
-  vid.removeAttribute("loop");
-  vid.removeAttribute("muted");
-  unmute();
-  vid.onended = function() {
-  vid.removeAttribute("src");
-  vid.setAttribute("src",'assets/video/00.mp4',);
-
-}};
-function goback2(){
-vid.removeAttribute("loop");
-vid.removeAttribute("muted");
-unmute();
-vid.onended = function() {
-vid.removeAttribute("src");
-vid.setAttribute("src",'assets/video/00.mp4',);
-
-}};
-
-function goback3(){
-vid.removeAttribute("loop");
-vid.removeAttribute("muted");
-unmute();
-vid.onended = function() {
-vid.removeAttribute("src");
-vid.setAttribute("src",'assets/video/00.mp4',);
-
-}};
-
-function goback4(){
-vid.removeAttribute("loop");
-vid.removeAttribute("muted");
-unmute();
-vid.onended = function() {
-vid.removeAttribute("src");
-vid.setAttribute("src",'assets/video/00.mp4',);
-
-}};
+$(document).ready(function(){
+  mainHeight = window.innerHeight - $(".navbar").height();
+  $("#landing").css("min-height", mainHeight);
+  $("#intro").css("height", window.innerHeight);
+  $("#set").css("height", window.innerHeight);
+  $("#reactions").css("height", window.innerHeight);
+  $("#project").css("height", window.innerHeight);
+  $("#tips").css("height", window.innerHeight);
+  $("#involve").css("height", window.innerHeight);
+  $(window).resize(function() {
+    $("#landing").css("min-height", mainHeight);
+    $("#intro").css("height", window.innerHeight);
+    $("#set").css("height", window.innerHeight);
+    $("#reactions").css("height", window.innerHeight);
+    $("#project").css("height", window.innerHeight);
+    $("#tips").css("height", window.innerHeight);
+    $("#involve").css("height", window.innerHeight);
+  });
+})
