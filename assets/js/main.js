2dd3f486d93fc03edef42cd330d3d3f3ae78adcb
ndrcldrll <andre.barbosa@mail.polimi.it>
2021-02-26 00:15:49 +0100

    3

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,9 @@ $(document).ready(function(){
   $("#reactions").css("min-height", window.innerHeight);
   $("#project").css("min-height", window.innerHeight);
   $("#tips").css("min-height", window.innerHeight);
-  $("#involve").css("min-height", window.innerHeight);
+  $("#get_involved").css("min-height", window.innerHeight);
   $("#download").css("min-height", window.innerHeight/2);
+//  $(".sticker").css("height", $(".sticker").width());
   $(window).resize(function() {
     mainHeight = window.innerHeight - $(".navbar").height();
     $("#landing").css("min-height", mainHeight);
@@ -16,7 +17,7 @@ $(document).ready(function(){
     $("#reactions").css("min-height", window.innerHeight);
     $("#project").css("min-height", window.innerHeight);
     $("#tips").css("min-height", window.innerHeight);
-    $("#involve").css("min-height", window.innerHeight);
+    $("#get_involved").css("min-height", window.innerHeight);
     $("#download").css("min-height", window.innerHeight/2);
   });
 })
