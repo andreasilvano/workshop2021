3ac8aa2fe2385178fcbf1e47a2adc1335cca1c5d
ndrcldrll <andre.barbosa@mail.polimi.it>
2021-02-25 21:47:40 +0100

    2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
   mainHeight = window.innerHeight - $(".navbar").height();
   $("#landing").css("min-height", mainHeight);
-  $("#intro").css("height", window.innerHeight);
-  $("#set").css("height", window.innerHeight);
-  $("#reactions").css("height", window.innerHeight);
-  $("#project").css("height", window.innerHeight);
-  $("#tips").css("height", window.innerHeight);
-  $("#involve").css("height", window.innerHeight);
-  $("#download").css("height", window.innerHeight/2);
+  $("#intro").css("min-height", window.innerHeight);
+  $("#set").css("min-height", window.innerHeight);
+  $("#reactions").css("min-height", window.innerHeight);
+  $("#project").css("min-height", window.innerHeight);
+  $("#tips").css("min-height", window.innerHeight);
+  $("#involve").css("min-height", window.innerHeight);
+  $("#download").css("min-height", window.innerHeight/2);
   $(window).resize(function() {
     mainHeight = window.innerHeight - $(".navbar").height();
     $("#landing").css("min-height", mainHeight);
-    $("#intro").css("height", window.innerHeight);
-    $("#set").css("height", window.innerHeight);
-    $("#reactions").css("height", window.innerHeight);
-    $("#project").css("height", window.innerHeight);
-    $("#tips").css("height", window.innerHeight);
-    $("#involve").css("height", window.innerHeight);
-    $("#download").css("height", window.innerHeight/2);
+    $("#intro").css("min-height", window.innerHeight);
+    $("#set").css("min-height", window.innerHeight);
+    $("#reactions").css("min-height", window.innerHeight);
+    $("#project").css("min-height", window.innerHeight);
+    $("#tips").css("min-height", window.innerHeight);
+    $("#involve").css("min-height", window.innerHeight);
+    $("#download").css("min-height", window.innerHeight/2);
   });
 })
