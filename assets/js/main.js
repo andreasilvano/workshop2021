c82b8c85f5ad8bdfc207622b13f966167181020a
ndrcldrll <andre.barbosa@mail.polimi.it>
2021-02-25 18:27:26 +0100

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,9 @@ $(document).ready(function(){
   $("#project").css("height", window.innerHeight);
   $("#tips").css("height", window.innerHeight);
   $("#involve").css("height", window.innerHeight);
+  $("#download").css("height", window.innerHeight/2);
   $(window).resize(function() {
+    mainHeight = window.innerHeight - $(".navbar").height();
     $("#landing").css("min-height", mainHeight);
     $("#intro").css("height", window.innerHeight);
     $("#set").css("height", window.innerHeight);
@@ -15,5 +17,6 @@ $(document).ready(function(){
     $("#project").css("height", window.innerHeight);
     $("#tips").css("height", window.innerHeight);
     $("#involve").css("height", window.innerHeight);
+    $("#download").css("height", window.innerHeight/2);
   });
 })
